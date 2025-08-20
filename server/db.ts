7bc3f116b80b28faa1ba35b7b63fa1db1302cfaf
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-08-20 18:29:44 +0100

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -11,5 +11,9 @@ if (!process.env.DATABASE_URL) {
   );
 }
 
-export const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+export const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false } // bypass self-signed cert
+});
+
 export const db = drizzle({ client: pool, schema });
