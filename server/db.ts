0e67fe05a06cd2fd451f317658d2ed04f5afe068
Toluwase Ayomide <167195927+Toludayo75@users.noreply.github.com>
2025-08-20 18:43:13 +0100

    Update 2 db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,19 +1,23 @@
-import { Pool, neonConfig } from '@neondatabase/serverless';
-import { drizzle } from 'drizzle-orm/neon-serverless';
+import fs from "fs";
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { drizzle } from "drizzle-orm/neon-serverless";
 import ws from "ws";
 import * as schema from "@shared/schema";
 
 neonConfig.webSocketConstructor = ws;
 
 if (!process.env.DATABASE_URL) {
-  throw new Error(
-    "DATABASE_URL must be set. Did you forget to provision a database?",
-  );
+  throw new Error("DATABASE_URL must be set. Did you forget to provision a database?");
 }
+
+// read the CA file
+const ca = fs.readFileSync("certs/ca.pem").toString();
 
 export const pool = new Pool({ 
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false } // bypass self-signed cert
+  ssl: {
+    ca, // use your downloaded CA certificate
+  }
 });
 
 export const db = drizzle({ client: pool, schema });
